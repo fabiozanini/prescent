a01c9ff96f8e56555e978a8db2ff3fd0737a877c
Fabio Zanini <fabio.zanini@tuebingen.mpg.de>
2015-09-07 12:35:24 +0200

    Basically works, still command line arg parsing

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,36 @@
+var fs = require('fs');
+var YAML = require('yamljs');
 var gulp = require('gulp');
+var rename = require('gulp-rename');
+var mustache = require('gulp-mustache');
+
 
 paths = {
-  presentation: 'presentation/*.html'
+  presentation: "./presentation/"
 }
 
 gulp.task('presentation', function() {
-  return gulp.src(paths.presentation)
-    .pipe(gulp.dest('build'));
+  var config = YAML.load(paths.presentation+'config.yml'),
+      slides = config.slides.map(function(x) {
+          return fs.readFileSync(paths.presentation+x+'.html');
+      });
+
+  var template = paths.presentation+'template.mustache'
+      view = {
+          title: config.title,
+          author: config.author,
+          description: config.description,
+          slides: slides.join('\n\n'),
+          revealjs: __dirname+'/node_modules/reveal.js/',
+      };
+  return gulp.src(template)
+             .pipe(rename('index.html'))
+             .pipe(mustache(view))
+             .pipe(gulp.dest(paths.presentation));
 });
 
 gulp.task('watch', function() {
-  gulp.watch(paths.presentation, ['presentation']);
+  gulp.watch(paths.presentation+'*', ['presentation']);
 });
 
 gulp.task('default', ['presentation', 'watch']);
